a388b23d4f5a24d7ea846933db6c0f996769eb31
Guillermo Galvan <guillermo.galvan@cotiviti.com>
2024-04-02 19:37:35 -0600

    Add fix logs cloudwatch

--- logger/cloudwatch.go
+++ logger/cloudwatch.go
@@ -7,7 +7,6 @@ import (
     "time"
 
     "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/awserr"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
@@ -32,27 +31,33 @@ func NewLogger(isDebug bool) *Logger {
 }
 
 func (l *Logger) Log(logGroupName, logStreamName, message string) {
-    if l.isDebug {
+	if l.isDebug {
         log.Printf("[DEBUG] %s", message)
         return
     }
 
     // Check if the log group exists
-    _, err := l.cwLogger.DescribeLogGroups(&cloudwatchlogs.DescribeLogGroupsInput{
+    describeLogGroupsOutput, err := l.cwLogger.DescribeLogGroups(&cloudwatchlogs.DescribeLogGroupsInput{
         LogGroupNamePrefix: aws.String(logGroupName),
     })
+
+    fmt.Println("Checking if log group exists end", err, describeLogGroupsOutput, len(describeLogGroupsOutput.LogGroups))
+
     if err != nil {
-        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == cloudwatchlogs.ErrCodeResourceNotFoundException {
-            // Log group doesn't exist, create it
-            _, err = l.cwLogger.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
-                LogGroupName: aws.String(logGroupName),
-            })
-            if err != nil {
-                fmt.Println("Error creating log group:", err)
-                os.Exit(1)
-            }
-        } else {
-            fmt.Println("Error describing log groups:", err)
+        fmt.Println("Error describing log groups:", err)
+        os.Exit(1)
+    }
+
+    if len(describeLogGroupsOutput.LogGroups) == 0 {
+        // Log group doesn't exist, create it
+        createLogGroupOutput, err := l.cwLogger.CreateLogGroup(&cloudwatchlogs.CreateLogGroupInput{
+            LogGroupName: aws.String(logGroupName),
+        })
+
+        fmt.Println("Log group created", createLogGroupOutput)
+
+        if err != nil {
+            fmt.Println("Error creating log group:", err)
             os.Exit(1)
         }
     }
@@ -69,6 +74,7 @@ func (l *Logger) Log(logGroupName, logStreamName, message string) {
             },
         },
     })
+
     if err != nil {
         fmt.Println("Error logging to CloudWatch:", err)
         os.Exit(1)
